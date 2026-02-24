78733cbb20a043f5e6d0142d6a12404a45d16570
shankar <shankarpy100@gmail.com>
2026-02-24 10:15:06 +0530

    Fresh clean commit without large files

---
+++ footer.php
@@ -0,0 +1,126 @@
+ <!-- START FOOTER -->
+        <footer class="first-footer">
+            <div class="top-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-6">
+                            <div class="netabout">
+                                <a href="index.html" class="logo">
+                                    <img src="images/logo-footer.svg" alt="netcom">
+                                </a>
+                                <p style="color:white;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum incidunt architecto soluta laboriosam, perspiciatis, aspernatur officiis esse.</p>
+                            </div>
+                            <div class="contactus">
+                                <ul>
+                                    <li>
+                                        <div class="info">
+                                            <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                            <p class="in-p" style="color:white;">Office:# 10, 2nd floor, Eureka Corner, Koppikar Road, Hubli-20</p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="info">
+                                            <i class="fa fa-phone" aria-hidden="true"></i>
+                                            <p class="in-p" style="color:white;">9538886974</p>
+                                            <p class="in-p" style="color:white;">9886601044</p>
+                                            <p class="in-p" style="color:white;">8050153337</p>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="info">
+                                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                                            <p class="in-p ti" style="color:white;">kitturweb@gmail.com</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="navigation">
+                                <h3  style="color:white;">Navigation</h3>
+                                <div class="nav-footer">
+                                    <ul>
+                                        <li><a href="index.html" style="color:white;">Home One</a></li>
+                                        <li><a href="properties-right-sidebar.html" style="color:white;">Properties Right</a></li>
+                                        <li><a href="properties-full-list.html" style="color:white;">Properties List</a></li>
+                                        <li><a href="properties-details.html" style="color:white;">Property Details</a></li>
+                                        <li class="no-mgb"><a href="agents-listing-grid.html" style="color:white;">Agents Listing</a></li>
+                                    </ul>
+                                    <ul class="nav-right">
+                                        <li><a href="agent-details.html" style="color:white;">Agents Details</a></li>
+                                        <li><a href="about.html" style="color:white;">About Us</a></li>
+                                        <li><a href="blog.html" style="color:white;">Blog Default</a></li>
+                                        <li><a href="blog-details.html" style="color:white;">Blog Details</a></li>
+                                        <li class="no-mgb"><a href="contact-us.html" style="color:white;">Contact Us</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="widget">
+                                <h3 style="color:white;">Twitter Feeds</h3>
+                                <div class="twitter-widget contuct">
+                                    <div class="twitter-area">
+                                        <div class="single-item">
+                                            <div class="icon-holder">
+                                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="text">
+                                                <h5 style="color:white;"><a href="#" style="color:white;">@findhouses</a style="color:white;"> all share them with me baby said inspet.</h5>
+                                                <h4 style="color:white;">about 5 days ago</h4>
+                                            </div>
+                                        </div>
+                                        <div class="single-item">
+                                            <div class="icon-holder">
+                                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="text">
+                                                <h5 style="color:white;"><a href="#" style="color:white;">@findhouses</a> all share them with me baby said inspet.</h5>
+                                                <h4  style="color:white;">about 5 days ago</h4>
+                                            </div>
+                                        </div>
+                                        <div class="single-item">
+                                            <div class="icon-holder">
+                                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="text">
+                                                <h5 style="color:white;"><a href="#" style="color:white;">@findhouses</a> all share them with me baby said inspet.</h5>
+                                                <h4  style="color:white;">about 5 days ago</h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="newsletters">
+                                <h3 style="color:white;">Newsletters</h3>
+                                <p style="color:white;">Sign Up for Our Newsletter to get Latest Updates and Offers. Subscribe to receive news in your inbox.</p>
+                            </div>
+                            <form class="bloq-email mailchimp form-inline" method="post">
+                                <label for="subscribeEmail" class="error"></label>
+                                <div class="email">
+                                    <input type="email" id="subscribeEmail" name="EMAIL" placeholder="Enter Your Email">
+                                    <input type="submit" value="Subscribe">
+                                    <p class="subscription-success"></p>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="second-footer">
+                <div class="container">
+                    <p>2021 © Copyright - All Rights Reserved.</p>
+                    <ul class="netsocials">
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                        <li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+
+        <a data-scroll href="#wrapper" class="go-up"><i class="fa fa-angle-double-up" aria-hidden="true"></i></a>
+        <!-- END FOOTER -->
---
+++ header.php
@@ -0,0 +1,30 @@
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+<!-- FAVICON -->
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <!-- GOOGLE FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i%7CMontserrat:600,800" rel="stylesheet">
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="css/fontawesome-5-all.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <!-- Slider Revolution CSS Files -->
+    <link rel="stylesheet" href="revolution/css/settings.css">
+    <link rel="stylesheet" href="revolution/css/layers.css">
+    <link rel="stylesheet" href="revolution/css/navigation.css">
+    <!-- ARCHIVES CSS -->
+    <link rel="stylesheet" href="css/slider-home18.css">
+    <link rel="stylesheet" href="css/search.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/swiper.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/lightcase.css">
+    <link rel="stylesheet" href="css/owl-carousel.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" id="color" href="css/colors/light-black.css">
